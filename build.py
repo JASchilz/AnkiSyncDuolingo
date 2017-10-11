b6b6c4e7f680b0fae32e24f4910c148f3f35ab5d
Joseph Schilz <joseph@schilz.org>
2017-10-10 18:54:54 -0700

    Build zip dist.

---
+++ build.py
@@ -0,0 +1,23 @@
+import os
+from zipfile import ZipFile
+
+
+def project_files(path):
+
+    result = []
+    for root, dirs, files in os.walk(path):
+        result += [os.path.join(root, f) for f in files if '.pyc' not in f]
+    return result
+
+
+if __name__ == '__main__':
+    
+    with ZipFile('duolingo_sync.zip', 'w') as myzip:
+        myzip.write('duolingo_sync.py')
+        myzip.write('duolingo_sync')
+        
+        for f in project_files('duolingo_sync'):
+            myzip.write(f)
+
+
+
