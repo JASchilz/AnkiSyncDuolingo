b6b4c09fbee80eedcd1945f17fb0f04211afa974
Joseph Schilz <joseph@schilz.org>
2018-04-06 21:21:44 -0700

    Package up README and LICENSE. Closes #11.

--- build.py
+++ build.py
@@ -15,6 +15,8 @@ if __name__ == '__main__':
     with ZipFile('duolingo_sync.zip', 'w') as myzip:
         myzip.write('duolingo_sync.py')
         myzip.write('duolingo_sync')
+        myzip.write('README.md', 'duolingo_sync/README.md')
+        myzip.write('LICENSE.md', 'duolingo_sync/LICENSE.md')
         
         for f in project_files('duolingo_sync'):
             myzip.write(f)
